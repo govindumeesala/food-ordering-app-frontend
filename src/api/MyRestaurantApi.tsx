c20da9760e9f06e6d0c07db7276498edf6a6bc5d
govindumeesala <meesalagovindu123@gmail.com>
2024-07-19 00:05:37 +0530

    bug clearing

--- src/api/MyRestaurantApi.tsx
+++ src/api/MyRestaurantApi.tsx
@@ -2,7 +2,6 @@ import { Order, Restaurant } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
-import { json } from "stream/consumers";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
